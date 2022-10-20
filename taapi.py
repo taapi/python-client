c1b010f5a1ecf8164345488bb3ca488bda2c6079
brianvilhelmsen <bv@taapi.io>
2022-10-20 17:18:41 +0200

    Initial first working version

--- taapi.py
+++ taapi.py
@@ -33,6 +33,80 @@ class Single :
 
         result = json.loads(data.decode("utf-8"))
 
-        print(result["value"])
+        return result
     
     
+class Bulk :
+
+    # Properties
+    def __init__(self, secret) :
+        self.secret = secret
+        self.outputFormat = "objects"
+        self.constructs = []
+
+    # Reset constructs
+    def initConstructs(self) :
+        self.constructs = []
+
+    # Set output format [default, objects]
+    def setOutputFormat(self, outputFormat) :
+        self.outputFormat = outputFormat
+
+    # Add a construct
+    def addConstruct(self, construct) :
+        self.constructs.append(construct.generate())
+
+    # Execute: calculate indicator values
+    def execute(self) :
+        
+        query = json.dumps({
+            "secret": self.secret,
+            "outputFormat": self.outputFormat,
+            "construct": self.constructs[0] if len(self.constructs) == 1 else self.constructs
+        })
+
+        conn = http.client.HTTPSConnection("api.taapi.io")
+
+        headers = { 'Content-Type': "application/json" }
+
+        conn.request("POST", "/bulk", query, headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        result = json.loads(data.decode("utf-8"))
+
+        return result
+    
+class Construct :
+
+    # Constructor
+    def __init__(self, exchange, symbol, interval) :
+        self.exchange = exchange;
+        self.symbol = symbol;
+        self.interval = interval;
+        self.indicators = []
+
+    # Reset indicators
+    def initIndicators(self) :
+        self.indicators = []
+
+    # Add an indicator along with parameters
+    def addIndicator(self, indicator, params = {}) :
+
+        params.update({
+            "indicator": indicator
+        })
+
+        self.indicators.append(params)
+
+    # Generate construct
+    def generate(self) :
+        construct = {
+            "exchange": self.exchange,
+            "symbol": self.symbol,
+            "interval": self.interval,
+            "indicators": self.indicators
+        }
+
+        return construct
